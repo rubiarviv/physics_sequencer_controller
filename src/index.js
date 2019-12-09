1548f4131ab5a4317cc95e464e49470212210f13
Rubi.Arviv <rubi.arviv@clicksoftware.com>
2019-12-09 11:28:49 +0200

    master

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+// Name: Rubi Arviv
+// ID: 033906132
 import { initialize } from '@muzilator/sdk';
 
 var midiChannel;
 var selectedKnob='knob1';
 var knobsToCircles = {};
 var checked = false;
+var handleInterval=null;
 
 
 const circles = [
@@ -137,16 +140,7 @@ function draw() {
 
 
 var keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'a#', 'c#', 'd#', 'f#', 'g#'];
-var keysToMidi = {'C': 60,'C#':61,'D':62,'D#':63,'F':64,'F#':65,'G':66,'G#':67,'A':68,'A#':69,'B':70};
-
-// var synth = new AudioSynth;
-// synth.setVolume(0.1337); // Recommended volume setting
-// var piano = synth.createInstrument('piano');
-// var raygun = synth.createInstrument('raygun');
-// var acoustic = synth.createInstrument('acoustic');
-// var organ = synth.createInstrument('organ');
-// var instruments = [piano, acoustic, organ, raygun];
-// var instrument = 0;
+var keysToMidi = {'C': 60,'C#':61,'D':62,'D#':63,'E':64,'F':65,'F#':66,'G':67,'G#':68,'A':69,'A#':70,'B':71};
 
 function FireEvent(pitch)
 {
@@ -178,50 +172,6 @@ function Key(note, keyEl) {
             setTimeout(FireEvent,500,knobsToCircles[selectedKnob].id);
         });
     };
-    // this.keyBoard = function() {
-    //     document.addEventListener('keydown', function(event) {
-    //     event.preventDefault();
-    //     switch (event.code) {
-    //         case 'KeyA':
-    //         new newCircle('F');
-    //         break;
-    //         case 'KeyS':
-    //         new newCircle('G');
-    //         break;
-    //         case 'KeyD':
-    //         new newCircle('A');
-    //         break;
-    //         case 'KeyF':
-    //         new newCircle('B');
-    //         break;
-    //         case 'KeyJ':
-    //         new newCircle('C');
-    //         break;
-    //         case 'KeyK':
-    //         new newCircle('D');
-    //         break;
-    //         case 'KeyL':
-    //         new newCircle('E');
-    //         break;
-    //         case 'KeyW':
-    //         new newCircle('F#');
-    //         break;
-    //         case 'KeyE':
-    //         new newCircle('G#');
-    //         break;
-    //         case 'KeyR':
-    //         new newCircle('A#');
-    //         break;
-    //         case 'KeyI':
-    //         new newCircle('C#');
-    //         break;
-    //         case 'KeyO':
-    //         new newCircle('D#');
-    //         break;
-    //         default:
-    //     }
-    //     });
-    // }
 }
 
 for (var i = 0; i < keys.length; i++) {
@@ -237,39 +187,37 @@ function Knob(knobEl) {
       event.preventDefault();
       var knobClicked = event.target.id;
       switch (knobClicked) {
-        // Reverse spin direction
         case 'knob1':
             selectedKnob='knob1';
-          // angle*=-1;
           break;
-        // Remove side of square.
         case 'knob2':
             selectedKnob='knob2';
-          // removeSide();
           break;
-        // Instrument changer
         case 'knob3':
             selectedKnob='knob3';
-          // instrument = randNum(0,instruments.length);
           break;
         // Reset
         case 'knob4':
             selectedKnob='knob4';
-          // window.location.reload();
           break;
         case 'knob5':
           if(checked==false){
-            setInterval(draw,75);
+            handleInterval=setInterval(draw,75);
             checked = true
           }
-          // window.location.reload();
           break;
+        case 'knob6':
+            if(checked==true){
+              clearInterval(handleInterval);
+              checked = false
+            }
+            break;
       }
     });
   };
 }
 
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 6; i++) {
   var knob = new Knob(document.getElementById('knob'+(i+1)));
   knob.addListener();
 }
